c7327be967d44cb9513409d2a806b7d1155130bf
Tiago Zaccaro Dias Santana <tiagozaccaro@gmail.com>
2025-11-30 02:11:24 +0000

    feat(ui): add theme toggle to site header
    - Wrap app in ThemeProvider for theme management
    - Add ThemeToggle component to header for user theme switching
    - Update code style to use single quotes in imports and JSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from 'next-themes';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider attribute='class' defaultTheme='light' enableSystem={false}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
